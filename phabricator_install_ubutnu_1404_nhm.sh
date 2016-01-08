1b77d8f36a1aa738b0ff1fd7ce9414ff0b1422fa
xyezir <xyezir@gmail.com>
2016-01-08 14:52:17 +0800

    fixed nginx

--- phabricator_install_ubutnu_1404_nhm.sh
+++ phabricator_install_ubutnu_1404_nhm.sh
@@ -47,7 +47,7 @@ sudo apt-get install unzip vim curl wget build-essential python-software-propert
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
 sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
 
-sudo add-apt-repository -y ppa:nginx/mainline
+sudo apt-key add nginx_signing.key
 
 sudo apt-get -qq update
 sudo apt-get install \
